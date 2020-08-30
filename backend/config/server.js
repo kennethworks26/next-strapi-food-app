43495aea3fa9cc00c891bd0eb4d68772d426dc0c
Kenneth Fernandez <kennethworks26@gmail.com>
2020-08-31 01:53:29 +0800

    feat(starter): initialize boilerplate

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21c251532c7716806698b6d5a93b91f4'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a43b3666-1a3d-49c1-b535-26a62a0ad648'
+};
